76bc960db02118125713f78077e18d306efd34f5
Verlierer <sestrunay13@gmail.com>
2021-11-07 18:18:19 +0300

    Add tests

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,20 @@
+import junit.framework.Assert;
+import person.Person;
+import person.Sex;
+import org.testng.annotations.Test;
+
+import java.time.LocalDate;
+
+public class PersonTest {
+    Person person1 = new Person((long) 1, "Person1", LocalDate.of(2000, 5, 17), Sex.FEMALE, 2014664480);
+    Person person2 = new Person((long) 2, "Person2", LocalDate.of(1978, 10, 18), Sex.FEMALE, 2002600912);
+    Person person3 = new Person((long) 3, "Person3", LocalDate.of(2006, 8, 31), Sex.MALE, 2021628960);
+
+    @Test
+    public void testGetAge() {
+        Assert.assertEquals(21, person1.getAge());
+        Assert.assertEquals(43, person2.getAge());
+        Assert.assertEquals(15, person3.getAge());
+    }
+
+}
---
+++ src/test/java/RepositoryTest.java
@@ -0,0 +1,57 @@
+import contracts.Contract;
+import contracts.DigitalTVContract;
+import contracts.MobileContract;
+import contracts.WiredInternetContract;
+import junit.framework.Assert;
+import person.Person;
+import person.Sex;
+import org.testng.annotations.Test;
+import repository.Repository;
+
+import java.time.LocalDate;
+
+
+public class RepositoryTest {
+    Person person1 = new Person((long) 1, "Person1", LocalDate.of(2000, 5, 17), Sex.FEMALE, 2014664480);
+    Person person2 = new Person((long) 2, "Person2", LocalDate.of(1978, 10, 18), Sex.FEMALE, 2002600912);
+    Person person3 = new Person((long) 3, "Person3", LocalDate.of(2006, 8, 31), Sex.MALE, 2021628960);
+    Contract contract1 = new DigitalTVContract(1, LocalDate.of(2019, 12, 21), LocalDate.of(2025, 12, 21), 123, person1, new String[]{"1", "2"});
+    Contract contract2 = new MobileContract(2, LocalDate.of(2017, 5, 3), LocalDate.of(2022, 11, 30), 356, person2, 15, 13, 22);
+    Contract contract3 = new WiredInternetContract(3, LocalDate.of(2010, 7, 13), LocalDate.of(2040, 7, 13), 556, person3, 50);
+
+
+    @Test
+    public void testAdd() {
+        Repository repository = new Repository();
+        repository.add(contract1);
+        repository.add(contract2);
+        repository.add(contract3);
+        Assert.assertEquals(3, repository.getSize());
+    }
+
+    @Test
+    public void testGetById() {
+        Repository repository = new Repository();
+        repository.add(contract1);
+        repository.add(contract2);
+        repository.add(contract3);
+        Assert.assertEquals(contract1, repository.getById(1, DigitalTVContract.class));
+        Assert.assertEquals(contract2, repository.getById(2, MobileContract.class));
+        Assert.assertEquals(contract3, repository.getById(3, WiredInternetContract.class));
+    }
+
+    @Test
+    public void testRemoveById() {
+        Repository repository = new Repository();
+        repository.add(contract1);
+        repository.add(contract2);
+        repository.add(contract3);
+        repository.removeById(1);
+        Assert.assertNull(repository.getById(1, DigitalTVContract.class));
+        repository.removeById(2);
+        Assert.assertNull(repository.getById(2, DigitalTVContract.class));
+        repository.removeById(3);
+        Assert.assertNull(repository.getById(3, DigitalTVContract.class));
+        Assert.assertEquals(0, repository.getSize());
+    }
+}
